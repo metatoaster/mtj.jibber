b5bad1e3fca3f5c035f6f817c87274d74badcb14
Tommy Yu <y@metatoaster.com>
2013-07-20 17:59:08 +1200

    Stubs for loading and setting up clients.

--- mtj/jibber/core.py
+++ mtj/jibber/core.py
@@ -10,11 +10,18 @@ logger = logging.getLogger('mtj.jibber.core')
 class BotCore(object):
     """
     The base jabber bot class.
+
+    General use case might go something like this::
+
+        client.load_server_config(server_configs)
+        client.load_client_config(client_configs)
+        client.connect()
+
     """
 
     _client_cls = ClientXMPP
 
-    def __init__(self, s_config=None):
+    def __init__(self, s_config=None, c_config=None):
         self.jid = None
         self.password = None
         self.host = None
@@ -24,6 +31,9 @@ class BotCore(object):
 
         if s_config is not None:
             self.load_server_config(s_config)
+
+        if c_config is not None:
+            self.load_client_config(c_config)
 
     def load_server_config(self, s_config):
         """
@@ -42,11 +52,24 @@ class BotCore(object):
             return None
         return (self.host, self.port)
 
+    def load_client_config(self, c_config):
+        self.config = json.loads(s_config)
+
+    def setup_client(self, client):
+        """
+        To be customized by client implementations.
+        """
+
+    def make_client(self):
+        client = self._client_cls(self.jid, self.password)
+        self.setup_client(client)
+        return client
+
     def connect(self):
         if self.client is not None:
             logger.info('Bot appears to be connected.')
             return
-        client = self._client_cls(self.jid, self.password)
+        client = self.make_client()
         client.connect(address=self.address)
         client.process(block=False)
         self.client = client
