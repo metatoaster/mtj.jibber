0a7f356a2f5f107ed126ac91a5a5af3c0e326b63
Tommy Yu <y@metatoaster.com>
2013-11-08 00:05:35 +1300

    Allow callables.
    - Make these powerful enough to generate dynamic responses.

--- mtj/jibber/bot.py
+++ mtj/jibber/bot.py
@@ -35,36 +35,71 @@ class Fortune(Command):
 
 
 class ChanceGame(Command):
+    """
+    A game of chance.
+
+    Accepts a list of 2-tuple in the format of chance and response.
+
+    Chance should be ordered in incremental real numbers < 1.
+    Response is the message associated with the chance to trigger.
+
+    >>> cg = ChanceGame((
+    ...     (0.1, 'You are dead.'),
+    ...     (1, 'You live!'),
+    ... ))
+    >>> cg.play({'mucnick': 'Tester'}, None)
+    'You...'
+
+    The parameter can alternatively be a callable, too.
+
+    >>> def dummy(msg, match):
+    ...     return 'No way, %(mucnick)s.'
+    >>> cg = ChanceGame((
+    ...     (1, dummy),
+    ... ))
+    >>> cg.play({'mucnick': 'Tester'}, None)
+    'No way, Tester.'
+    """
 
     def __init__(self, chance_table):
-        """
-        List of 2-tuple in the format of chance and response.
-
-        Chance should be ordered in incremental real numbers < 1.
-        Response is the message associated with the chance to trigger.
-
-        >>> cg = ChanceGame((
-        ...     (0.1, 'You are dead.'),
-        ...     (1, 'You live!'),
-        ... ))
-        >>> cg.play({'mucnick': 'Tester'}, None)
-        'You...'
-        """
-
         self.chance_table = chance_table
 
     def play(self, msg, match):
         chance = random.random()
         for trigger, response in self.chance_table:
             if chance <= trigger:
+                if callable(response):
+                    response = response(msg, match)
                 return response % msg
         return ''
 
 
 class PickOne(Command):
+    """
+    Pick a response.
+
+    Accepts a list of strings or callables that will generate a
+    response.
+
+    >>> po = PickOne(('%(mucnick)s: red', '%(mucnick)s: green',
+    ...     '%(mucnick)s: blue'))
+    >>> po.play({'mucnick': 'Tester'}, None)
+    'Tester: ...'
+
+    The parameter can alternatively be a callable, too.
+
+    >>> def dummy(msg, match):
+    ...     return 'No way, %(mucnick)s.'
+    >>> po = PickOne((dummy,))
+    >>> po.play({'mucnick': 'Tester'}, None)
+    'No way, Tester.'
+    """
 
     def __init__(self, items):
         self.items = items
 
     def play(self, msg, match):
-        return random.choice(self.items)
+        result = random.choice(self.items)
+        if callable(result):
+            return result(msg, match) % msg
+        return result % msg
