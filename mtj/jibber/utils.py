16a2914509b2d53152f8a2bb93a1299f51308eff
Tommy Yu <y@metatoaster.com>
2013-11-08 23:03:51 +1300

    Doc and doctest.

--- mtj/jibber/utils.py
+++ mtj/jibber/utils.py
@@ -1,6 +1,19 @@
 import re
 
 def strip_tags(html):
+    """
+    Lazily strip all tags in a very lazy way.
+
+    >>> print(strip_tags(None))
+    None
+    >>> print(strip_tags('test string'))
+    test string
+    >>> print(strip_tags('test <b>string</b>'))
+    test string
+    >>> print(strip_tags('<test string>'))
+    <BLANKLINE>
+    """
+
     if html is None:
         return None
     
