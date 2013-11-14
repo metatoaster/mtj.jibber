96d0c81f07e6d0a6d7257bb32e23aebe925639aa
Tommy Yu <y@metatoaster.com>
2013-11-14 23:30:25 +1300

    Changelog updates for initial release.

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,8 +1,8 @@
 Changelog
 =========
 
-0.1-dev (unreleased)
---------------------
+0.1 - 2013-11-14
+----------------
 
 - Core functions implemented, including connect to XMPP server as a muc
   client, and to provide trigger handlers and dynamic module/object
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (
 
 setup(name='mtj.jibber',
       version=version,
-      description="A jabber bot",
+      description="A jabber bot framework.",
       long_description=long_description,
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
